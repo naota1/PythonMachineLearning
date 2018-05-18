3e7930883adab3278d215b2e8ef78bb6e590966c
naota <13465771+naota1@users.noreply.github.com>
2018-05-18 22:47:04 +0200

    initial commit

--- main.py
+++ main.py
@@ -38,3 +38,13 @@ print(dataset.groupby("class").size())
 print("*** printed class distribution ***")
 
 # box and whisker plots
+dataset.plot(kind="box", subplots=True, layout=(2, 2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
